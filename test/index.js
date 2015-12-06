24077c92749a71eb15ede043ce1bdf23a8784836
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-12-06 22:22:09 +0000

    Initial commit.

---
+++ test/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var test = require('tape');
+var m = require('..');
+
+test('dual-device', function (t) {
+    t.test('module fetches instance information', function (s) {
+        s.plan(1);
+        
+    });
+});
